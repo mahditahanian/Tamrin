4a735f689d22a8f3156059f422e07957e7d3aa98
Mahdi Tahanian <mahdi.tahanian@live.com>
2025-11-11 15:28:50 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+Parallel.For(0, 4, i =>
+{
+    int threadId = Thread.CurrentThread.ManagedThreadId;
+
+    var inst1 = FactorManager.Instance;
+    var inst2 = FactorManager.Instance;
+    var inst3 = FactorManager.Instance;
+
+    Console.WriteLine($"[Thread {threadId}] HashCodes: {inst1.GetHashCode()}, {inst2.GetHashCode()}, {inst3.GetHashCode()}");
+});
+
+
+
+
+
+
+
+
+
